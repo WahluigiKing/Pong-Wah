d9996209c870d7cb849644ffc6e9e9558beecdbe
WahluigiKing <itsame9411@gmail.com>
2021-05-10 19:04:29 -0400

    Day Two Of Production
    The second day of production of Wah-Pong, where we added code for the Start       Button.

---
+++ REAL Wah-Pong/Assets/Scripts/PlayButton.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayButton : MonoBehaviour
+{
+   public void PlayGame()
+    {
+        Debug.Log("Start was pressed");
+    }
+   
+}
