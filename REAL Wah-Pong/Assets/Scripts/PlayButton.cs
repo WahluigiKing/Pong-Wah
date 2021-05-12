ef4f232a397706d88df244ba4f7fa43470b86bd8
WahluigiKing <itsame9411@gmail.com>
2021-05-12 19:06:09 -0400

    Day Three Of Production
    The third day of production of Wah-Pong, where we finished the main menu, and started the game adding rackets, walls, net, and ball.

--- REAL Wah-Pong/Assets/Scripts/PlayButton.cs
+++ REAL Wah-Pong/Assets/Scripts/PlayButton.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayButton : MonoBehaviour
 {
    public void PlayGame()
     {
         Debug.Log("Start was pressed");
+
+        // Moves us to the Game scene.
+        SceneManager.LoadScene("Game");
     }
    
 }
